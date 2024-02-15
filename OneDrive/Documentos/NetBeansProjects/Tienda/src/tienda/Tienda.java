e62062b17b96b851ea80f0995aa14d746841ae86
Robzoly <roberto23zeta@gmail.com>
2024-02-15 14:38:47 -0600

    Creacion proyecto

---
+++ OneDrive/Documentos/NetBeansProjects/Tienda/src/tienda/Tienda.java
@@ -0,0 +1,14 @@
+
+package tienda;
+
+
+public class Tienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
